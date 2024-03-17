5888fc00207ac37b6f49ba047d2f34b6c62eeefb
milvusAquila <milvus.aquila@ik.me>
2024-03-17 18:36:27 +0100

    add default.json in assets

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,31 @@
 use iced::{
     executor,
-    widget::{button, column, container, horizontal_space, row, text, text_editor},
-    Application, Command, Element, Length, Settings, Theme,
+    widget::{button, column, container, horizontal_space, row, text, text_input},
+    Application, Command, Element, Settings, Theme,
 };
-use std::{
-    io,
-    path::{Path, PathBuf},
-    sync::Arc,
-};
+use std::{io, path::PathBuf, sync::Arc};
 
 fn main() -> iced::Result {
     App::run(Settings::default())
 }
 
 struct App {
-    content: text_editor::Content,
+    content: Vec<String>,
+    current: Option<u8>,
     error: Option<Error>,
-    path: Option<PathBuf>,
+    file: Option<PathBuf>,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    Edit(text_editor::Action),
-    Open,
+    TextInputChanged(String),
     FileOpened(Result<(PathBuf, Arc<String>), Error>),
 }
 
 #[derive(Debug, Clone)]
 enum Error {
+    IoError(io::ErrorKind),
     DialogClosed,
-    IO(io::ErrorKind),
 }
 
 impl Application for App {
@@ -41,58 +37,59 @@ impl Application for App {
     fn new(_flag: Self::Flags) -> (Self, Command<Message>) {
         (
             Self {
-                content: text_editor::Content::new(),
+                content: Vec::new(),
+                current: None,
                 error: None,
-                path: None,
+                file: None,
             },
-            Command::perform(load_file(default_file()), Message::FileOpened),
+            Command::perform(
+                load_file(default_file()),
+                Message::FileOpened,
+            ),
         )
     }
 
     fn title(&self) -> String {
-        String::from("My first app with iced")
+        String::from("Vocabulary")
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::Edit(action) => {
-                self.content.perform(action);
-                Command::none()
+            Message::TextInputChanged(value) => {
+                todo!();
             }
-            Message::Open => Command::perform(pick_file(), Message::FileOpened),
-            Message::FileOpened(Ok((path, content))) => {
-                self.path = Some(path);
-                self.content = text_editor::Content::with_text(&content);
-                Command::none()
-            }
-            Message::FileOpened(Err(error)) => {
-                self.error = Some(error);
+            Message::FileOpened(result) => {
+                if let Ok((path, contents)) = result {
+                    self.file = Some(path);
+                    self.content = todo!();
+                };
                 Command::none()
             }
         }
     }
 
     fn view(&self) -> Element<'_, Message> {
-        let controls = row![button("Open").on_press(Message::Open)];
-        let input = text_editor(&self.content)
-            .height(Length::Fill)
-            .on_action(Message::Edit);
-        let file_path = match self.path.as_deref().and_then(Path::to_str) {
-            Some(path) => text(path).size(14),
-            None => text(""),
-        };
-        let position = {
-            let (line, column) = self.content.cursor_position();
-            text(format!("{}:{}", line + 1, column + 1))
-        };
-        let status_bar = row![file_path, horizontal_space(), position];
+        // let controls = row![button("Open").on_press(Message::Open)];
+        // let input = text_editor(&self.content)
+        //     .height(Length::Fill)
+        //     .on_action(Message::Edit);
+        // let file_path = match self.path.as_deref().and_then(Path::to_str) {
+        //     Some(path) => text(path).size(14),
+        //     None => text(""),
+        // };
 
-        // let head_one = text("Français");
-        // let head_two = text("Anglais");
+        let head_one = text("Français");
+        let head_two = text("Anglais");
 
-        container(column![controls, input, status_bar].spacing(10))
-            .padding(10)
-            .into()
+        let input_one = text_input("Write your answer", &self.content[0]);
+        let input_two = text_input("Write your answer", &self.content[0]);
+
+        container(column![
+            row![head_one, input_one.on_input(Message::TextInputChanged)],
+            row![head_two, input_two.on_input(Message::TextInputChanged)],
+        ])
+        .padding(10)
+        .into()
     }
 
     fn theme(&self) -> Theme {
@@ -101,15 +98,14 @@ impl Application for App {
 }
 
 fn default_file() -> PathBuf {
-    PathBuf::from(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR")))
+    PathBuf::from(format!("{}/assets/default.json", env!("CARGO_MANIFEST_DIR")))
 }
 
 async fn load_file(path: PathBuf) -> Result<(PathBuf, Arc<String>), Error> {
     let contents = tokio::fs::read_to_string(&path)
         .await
         .map(Arc::new)
-        .map_err(|error| error.kind())
-        .map_err(Error::IO)?;
+        .map_err(|error| Error::IoError(error.kind()))?;
     Ok((path, contents))
 }
 
