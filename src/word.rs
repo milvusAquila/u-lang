90774aef98893bbb34d9bb45ce47be610bfa4b82
milvusAquila <milvus.aquila@ik.me>
2024-03-30 19:08:36 +0100

    add src/word.rs file

---
+++ src/word.rs
@@ -0,0 +1,33 @@
+pub struct Entry (pub Word, pub Word, pub GramClass);
+
+pub enum Word {
+    One(String),
+    List(Vec<String>),
+}
+
+pub enum GramClass {
+    Noun,
+    Verb,
+}
+
+impl Entry {
+    pub fn get(&self, lang: usize) -> String {
+        let word = &self.0;
+        match word {
+            Word::One(content) => content.to_string(),
+            Word::List(content) => {
+                let mut formatted = String::new();
+                for i in content {
+                    formatted += format!("{} / ", i).as_str();
+                }
+                formatted
+            }
+        }
+    }
+}
+
+impl Into<Word> for &str {
+    fn into(self) -> Word {
+        Word::One(String::from(self))
+    }
+}
